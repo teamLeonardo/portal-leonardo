85465e15863c898ba9a71cff469a9639b06559af
leonardo sifuentes <leonardosm3.14@gmail.com>
2022-04-24 05:31:08 -0500

    fix: correxion

--- pages/portafolio/[slug].js
+++ pages/portafolio/[slug].js
@@ -6,15 +6,15 @@ export default ({ data, error }) => {
 
   const { push } = useRouter()
   if (error && error.type === 404) {
-    push({ pathname: "/404" })
+    push("/404")
+    return null;
   }
-  return <SectionPortal title={data.title} content={data.content} />
+  return !error && data && <SectionPortal title={data.title} content={data.content} />
 }
 export async function getServerSideProps({ query }) {
   if (query && query.slug) {
     const { data } = await client.query({ query: querys.POST_PORTAFOLIO_BY_SLUG, variables: { slug: "" + query.slug } })
-    if (!data) return { error: { text: "pagina no encontrada", type: 404 } }
-
+    if (data && data.postBy === null) return { props: { error: { text: "pagina no encontrada", type: 404 } } }
     return { props: { data: data.postBy } }
   }
 }
